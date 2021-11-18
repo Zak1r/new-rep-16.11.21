64d7101ea8e167b7f075539121d3fa7e66824f8b
Zokirzhon Ikramzhanov <zokirzhon99@gmail.com>
2021-11-18 14:11:26 +0600

    6lesson

---
+++ index6.py
@@ -0,0 +1,80 @@
+# age = int(input('Enter your age: '))
+# # if age >18 and age <=50:     print('Welcome')
+# if 18<=age<=50: print ('Welcome')
+# else:   print('Bye')
+
+# num = int(input('Enter your number: '))
+# print(num%10)
+
+# num = int(input('Enter your number: '))
+# print(num // 1 % 10)
+# print(num // 10 % 10)
+# print(num // 100 % 10)
+# print(num // 1000 % 10)
+# print(num // 10000 % 10)
+# print(num // 100000 % 10)
+
+
+
+#t1
+'''num1= int(input('check: '))
+num2= int(input('your bill: '))
+num3= num2-num1
+if num2-num1==0:
+    print('invoice paid vithout change')
+elif num2%100>=num2-num1:
+    print(num1)
+else:
+    print('')'''
+
+# cost = int(input('check of order: '))
+# money = int(input('your bill: '))
+# # change = money - cost
+# if cost % 100 == 0 and money % 100 == 0:
+#     change = money - cost
+
+#     five_hundreds = change // 500
+#     change = change % 500
+
+#     two_hundreds = change // 200
+#     change = change % 200
+
+#     hundreds = change //100
+#     print(five_hundreds, two_hundreds, hundreds)
+
+#t2 
+# n1 = float(input('1st number: '))
+# n2 = float(input('2st number: '))
+# n3 = float(input('3st number: '))
+# if n1 < n2 < n3:
+#     print(n3, n2, n1)
+# if n2 < n1 < n3:
+#     print(n3, n1, n2)
+# if n3 < n2 < n1:
+#     print(n1, n2, n3)
+# if n1 < n3 < n2:
+#     print(n2, n3, n1)
+# if n2 < n3 < n1:
+#     print(n1, n3, n2)
+# if n3 < n1 < n2:
+#     print(n2, n1, n3)
+# if n1 == n2 == n3: print ('0')
+# print(n1, n2, n3)
+
+
+# a = float(input('1st number: '))
+# b = float(input('2st number: '))
+# c = float(input('3st number: '))
+
+# large = max (a, b, c)
+# small = min (a, b, c)
+# middle = a + b + c - large - small
+# print(large, middle, small)
+
+
+
+# дз
+# написать сортироку 4 чисел if 
+
+# написать программму какая запрашивает чисо. Если число положит то вывести его на квадарат а если иначе извлечь конеь 3 степеени
+
